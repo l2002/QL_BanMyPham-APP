60c6841d1db513e849fb46d28f4f028e2bb31636
pti2002 <145414043+pti2002@users.noreply.github.com>
2023-11-30 13:46:18 +0700

    Add files via upload

---
+++ QL_BanMyPham_APP/frmThuongHieu.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace QL_BanMyPham_APP
+{
+    partial class frmThuongHieu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtTenTH = new System.Windows.Forms.TextBox();
+            this.txtMaTH = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvThuongHieu = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThuongHieu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(1186, 142);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(199, 33);
+            this.btnXoa.TabIndex = 38;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(825, 142);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(199, 33);
+            this.btnSua.TabIndex = 39;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(446, 142);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(199, 33);
+            this.btnThem.TabIndex = 40;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtTenTH
+            // 
+            this.txtTenTH.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenTH.Location = new System.Drawing.Point(1299, 26);
+            this.txtTenTH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTenTH.Name = "txtTenTH";
+            this.txtTenTH.Size = new System.Drawing.Size(199, 32);
+            this.txtTenTH.TabIndex = 35;
+            // 
+            // txtMaTH
+            // 
+            this.txtMaTH.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaTH.Location = new System.Drawing.Point(550, 23);
+            this.txtMaTH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMaTH.Name = "txtMaTH";
+            this.txtMaTH.Size = new System.Drawing.Size(209, 32);
+            this.txtMaTH.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label6.Location = new System.Drawing.Point(1095, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 26);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Tên Thương Hiệu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(354, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 26);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Mã Thương Hiệu";
+            // 
+            // dgvThuongHieu
+            // 
+            this.dgvThuongHieu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThuongHieu.Location = new System.Drawing.Point(66, 187);
+            this.dgvThuongHieu.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvThuongHieu.Name = "dgvThuongHieu";
+            this.dgvThuongHieu.RowHeadersWidth = 51;
+            this.dgvThuongHieu.Size = new System.Drawing.Size(1793, 750);
+            this.dgvThuongHieu.TabIndex = 27;
+            this.dgvThuongHieu.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThuongHieu_CellClick);
+            // 
+            // frmThuongHieu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtTenTH);
+            this.Controls.Add(this.txtMaTH);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvThuongHieu);
+            this.Name = "frmThuongHieu";
+            this.Text = "frmThuongHieu";
+            this.Load += new System.EventHandler(this.frmThuongHieu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThuongHieu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtTenTH;
+        private System.Windows.Forms.TextBox txtMaTH;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvThuongHieu;
+    }
+}
---
+++ QL_BanMyPham_APP/frmThuongHieu.cs
@@ -0,0 +1,132 @@
+﻿using BLL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_BanMyPham_APP
+{
+    public partial class frmThuongHieu : Form
+    {
+        ThuongHieu thDTO = new ThuongHieu();
+        ThuongHieu_BLL thBLL = new ThuongHieu_BLL();
+        public frmThuongHieu()
+        {
+            InitializeComponent();
+            txtMaTH.Enabled = false;
+        }
+        private void frmThuongHieu_Load(object sender, EventArgs e)
+        {
+            loadTable();
+        }
+        private void loadTable()
+        {
+            dgvThuongHieu.DataSource = thBLL.GetListThuongHieu();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (checkTextBox())
+                MessageBox.Show("Vui lòng nhập tên thương hiệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                int stt = dgvThuongHieu.RowCount;
+                string math;
+                if (stt < 10)
+                {
+                    math = "TH" + "0" + stt.ToString();
+                }
+                else
+                {
+                    math = "TH" + stt.ToString();
+                }
+                thDTO.MaTH = math;
+                thDTO.TenTH = txtTenTH.Text;
+                if (thBLL.themThuongHieu(thDTO) != -1)
+                {
+                    MessageBox.Show("Thêm thành công", "Thông báo", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Thêm thất bại", "Thông báo", MessageBoxButtons.OK);
+                }
+                loadTable();
+            }
+        }
+        private bool checkTextBox()
+        {
+            if (txtTenTH.Text == "" )
+                return true;
+            return false;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (txtMaTH.Text == "")
+            {
+                MessageBox.Show("Vui lòng chọn dòng cần sửa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                thDTO.MaTH = txtMaTH.Text;
+                thDTO.TenTH = txtTenTH.Text;
+                if (thBLL.suaThuongHieu(thDTO) != -1)
+                {
+                    MessageBox.Show("Sửa thành công", "Thông báo", MessageBoxButtons.OK);
+                    txtMaTH.Clear();
+                    txtTenTH.Clear();
+                    txtTenTH.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Sửa thất bại", "Thông báo", MessageBoxButtons.OK);
+                }
+                loadTable();
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (txtMaTH.Text == "")
+            {
+                MessageBox.Show("Vui lòng chọn dòng cần xóa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var n = MessageBox.Show("Bạn có chắc muốn xóa?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (n == DialogResult.Yes)
+                {
+                    if (thBLL.xoaThuongHieu(txtMaTH.Text) != -1)
+                    {
+                        MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK);
+                        txtMaTH.Clear();
+                        txtTenTH.Clear();
+                        txtTenTH.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xóa thất bại", "Thông báo", MessageBoxButtons.OK);
+                    }
+                    loadTable();
+                }
+            }
+        }
+
+        private void dgvThuongHieu_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvThuongHieu.SelectedRows.Count > 0)
+            {
+                txtMaTH.Text = dgvThuongHieu.SelectedRows[0].Cells[0].Value.ToString();
+                txtTenTH.Text = dgvThuongHieu.SelectedRows[0].Cells[1].Value.ToString();
+                
+            }
+            dgvThuongHieu.ReadOnly = true;
+        }
+    }
+}
