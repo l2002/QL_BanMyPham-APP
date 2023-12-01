12a8b97257074176df064469988f17802c6ffc3d
pti2002 <145414043+pti2002@users.noreply.github.com>
2023-12-01 10:07:35 +0700

    Update frmThuongHieu.Designer.cs

--- QL_BanMyPham_APP/frmThuongHieu.Designer.cs
+++ QL_BanMyPham_APP/frmThuongHieu.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QL_BanMyPham_APP
+namespace QL_BanMyPham_APP
 {
     partial class frmThuongHieu
     {
@@ -36,16 +36,25 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dgvThuongHieu = new System.Windows.Forms.DataGridView();
+            this.btnSuaNCC = new System.Windows.Forms.Button();
+            this.btnThemNCC = new System.Windows.Forms.Button();
+            this.txtTenNCC = new System.Windows.Forms.TextBox();
+            this.txtMaNCC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvNCC = new System.Windows.Forms.DataGridView();
+            this.btnXoaNCC = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvThuongHieu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNCC)).BeginInit();
             this.SuspendLayout();
             // 
             // btnXoa
             // 
             this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnXoa.Location = new System.Drawing.Point(1186, 142);
+            this.btnXoa.Location = new System.Drawing.Point(674, 157);
             this.btnXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(199, 33);
+            this.btnXoa.Size = new System.Drawing.Size(158, 33);
             this.btnXoa.TabIndex = 38;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
@@ -54,10 +63,10 @@
             // btnSua
             // 
             this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSua.Location = new System.Drawing.Point(825, 142);
+            this.btnSua.Location = new System.Drawing.Point(339, 157);
             this.btnSua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(199, 33);
+            this.btnSua.Size = new System.Drawing.Size(170, 33);
             this.btnSua.TabIndex = 39;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
@@ -66,10 +75,10 @@
             // btnThem
             // 
             this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThem.Location = new System.Drawing.Point(446, 142);
+            this.btnThem.Location = new System.Drawing.Point(71, 157);
             this.btnThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(199, 33);
+            this.btnThem.Size = new System.Drawing.Size(139, 33);
             this.btnThem.TabIndex = 40;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
@@ -78,16 +87,16 @@
             // txtTenTH
             // 
             this.txtTenTH.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTenTH.Location = new System.Drawing.Point(1299, 26);
+            this.txtTenTH.Location = new System.Drawing.Point(270, 79);
             this.txtTenTH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTenTH.Name = "txtTenTH";
-            this.txtTenTH.Size = new System.Drawing.Size(199, 32);
+            this.txtTenTH.Size = new System.Drawing.Size(325, 32);
             this.txtTenTH.TabIndex = 35;
             // 
             // txtMaTH
             // 
             this.txtMaTH.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMaTH.Location = new System.Drawing.Point(550, 23);
+            this.txtMaTH.Location = new System.Drawing.Point(262, 21);
             this.txtMaTH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMaTH.Name = "txtMaTH";
             this.txtMaTH.Size = new System.Drawing.Size(209, 32);
@@ -98,7 +107,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.label6.Location = new System.Drawing.Point(1095, 29);
+            this.label6.Location = new System.Drawing.Point(66, 82);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(177, 26);
             this.label6.TabIndex = 31;
@@ -109,7 +118,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.label2.Location = new System.Drawing.Point(354, 26);
+            this.label2.Location = new System.Drawing.Point(66, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(171, 26);
             this.label2.TabIndex = 32;
@@ -118,20 +127,116 @@
             // dgvThuongHieu
             // 
             this.dgvThuongHieu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvThuongHieu.Location = new System.Drawing.Point(66, 187);
+            this.dgvThuongHieu.Location = new System.Drawing.Point(71, 202);
             this.dgvThuongHieu.Margin = new System.Windows.Forms.Padding(4);
             this.dgvThuongHieu.Name = "dgvThuongHieu";
             this.dgvThuongHieu.RowHeadersWidth = 51;
-            this.dgvThuongHieu.Size = new System.Drawing.Size(1793, 750);
+            this.dgvThuongHieu.Size = new System.Drawing.Size(761, 750);
             this.dgvThuongHieu.TabIndex = 27;
             this.dgvThuongHieu.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThuongHieu_CellClick);
+            // 
+            // btnSuaNCC
+            // 
+            this.btnSuaNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuaNCC.Location = new System.Drawing.Point(1425, 154);
+            this.btnSuaNCC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSuaNCC.Name = "btnSuaNCC";
+            this.btnSuaNCC.Size = new System.Drawing.Size(170, 33);
+            this.btnSuaNCC.TabIndex = 46;
+            this.btnSuaNCC.Text = "Sửa";
+            this.btnSuaNCC.UseVisualStyleBackColor = true;
+            this.btnSuaNCC.Click += new System.EventHandler(this.btnSuaNCC_Click);
+            // 
+            // btnThemNCC
+            // 
+            this.btnThemNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemNCC.Location = new System.Drawing.Point(1126, 154);
+            this.btnThemNCC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnThemNCC.Name = "btnThemNCC";
+            this.btnThemNCC.Size = new System.Drawing.Size(139, 33);
+            this.btnThemNCC.TabIndex = 47;
+            this.btnThemNCC.Text = "Thêm";
+            this.btnThemNCC.UseVisualStyleBackColor = true;
+            this.btnThemNCC.Click += new System.EventHandler(this.btnThemNCC_Click);
+            // 
+            // txtTenNCC
+            // 
+            this.txtTenNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenNCC.Location = new System.Drawing.Point(1348, 76);
+            this.txtTenNCC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTenNCC.Name = "txtTenNCC";
+            this.txtTenNCC.Size = new System.Drawing.Size(325, 32);
+            this.txtTenNCC.TabIndex = 44;
+            // 
+            // txtMaNCC
+            // 
+            this.txtMaNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaNCC.Location = new System.Drawing.Point(1348, 18);
+            this.txtMaNCC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMaNCC.Name = "txtMaNCC";
+            this.txtMaNCC.Size = new System.Drawing.Size(209, 32);
+            this.txtMaNCC.TabIndex = 45;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(1121, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 26);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Tên Nhà Cung Cấp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(1121, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 26);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Mã Nhà cung cấp";
+            // 
+            // dgvNCC
+            // 
+            this.dgvNCC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNCC.Location = new System.Drawing.Point(1126, 199);
+            this.dgvNCC.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvNCC.Name = "dgvNCC";
+            this.dgvNCC.RowHeadersWidth = 51;
+            this.dgvNCC.Size = new System.Drawing.Size(761, 750);
+            this.dgvNCC.TabIndex = 41;
+            this.dgvNCC.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvNCC_CellClick);
+            // 
+            // btnXoaNCC
+            // 
+            this.btnXoaNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoaNCC.Location = new System.Drawing.Point(1729, 154);
+            this.btnXoaNCC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnXoaNCC.Name = "btnXoaNCC";
+            this.btnXoaNCC.Size = new System.Drawing.Size(158, 33);
+            this.btnXoaNCC.TabIndex = 48;
+            this.btnXoaNCC.Text = "Xóa";
+            this.btnXoaNCC.UseVisualStyleBackColor = true;
+            this.btnXoaNCC.Click += new System.EventHandler(this.btnXoaNCC_Click);
             // 
             // frmThuongHieu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.btnXoaNCC);
+            this.Controls.Add(this.btnSuaNCC);
+            this.Controls.Add(this.btnThemNCC);
+            this.Controls.Add(this.txtTenNCC);
+            this.Controls.Add(this.txtMaNCC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvNCC);
             this.Controls.Add(this.btnXoa);
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnThem);
@@ -144,6 +249,7 @@
             this.Text = "frmThuongHieu";
             this.Load += new System.EventHandler(this.frmThuongHieu_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvThuongHieu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNCC)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,5 +265,13 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView dgvThuongHieu;
+        private System.Windows.Forms.Button btnSuaNCC;
+        private System.Windows.Forms.Button btnThemNCC;
+        private System.Windows.Forms.TextBox txtTenNCC;
+        private System.Windows.Forms.TextBox txtMaNCC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvNCC;
+        private System.Windows.Forms.Button btnXoaNCC;
     }
 }
