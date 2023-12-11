6783e95a826ef9dd33a4029f4649e1b60af77f10
pti2002 <145414043+pti2002@users.noreply.github.com>
2023-12-11 11:59:18 +0700

    Update KhachHangAccesss.cs

--- DAL/KhachHangAccesss.cs
+++ DAL/KhachHangAccesss.cs
@@ -13,4 +13,33 @@ namespace DAL
         DatabaseAccess database = new DatabaseAccess();
         
     }
+     public DataTable getKhachHang()
+     {
+         KhachHang kh = new KhachHang();
+         DataTable dt = database.fillTable("select * from KhachHang");
+         dt.Columns[0].ColumnName = "Mã khách hàng";
+         dt.Columns[1].ColumnName = "Tên khách hàng";
+         dt.Columns[2].ColumnName = "Địa chỉ";
+         dt.Columns[3].ColumnName = "Điện thoại";
+         dt.Columns[4].ColumnName = "Ngày sinh";
+         return dt;
+     }
+     public int themKhachHang(KhachHang kh)
+     {
+         string sql = "insert into KhachHang values('" + kh.MaKH + "',N'" + kh.TenKH + "',N'" + kh.DiaChi + "','" + kh.DienThoai + "','" + kh.NgaySinh + "')";
+         int kq = database.excuteNonQuery(sql);
+         return kq;
+     }
+     public int suaKhachHang(KhachHang kh)
+     {
+         string sql = "update KhachHang set tenkh=N'" + kh.TenKH + "',diachi=N'" + kh.DiaChi + "',dienthoai='" + kh.DienThoai + "',ngaysinh='" + kh.NgaySinh + "' where makh = '" + kh.MaKH + "'";
+         int kq = database.excuteNonQuery(sql);
+         return kq;
+     }
+     public int xoaKhachHang(string makh)
+     {
+         string sql = "delete KhachHang where makh = '" + makh + "'";
+         int kq = database.excuteNonQuery(sql);
+         return kq;
+     }
 }
